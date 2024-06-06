be0accab32fa6ff80f538958c97816b3c67d57a1
Irrlichter <ftuertinov@proton.me>
2024-06-06 21:57:21 +0200

    still doesn't work php-fpm does work

--- srcs/requirements/wordpress/conf/docker-entrypoint.sh
+++ srcs/requirements/wordpress/conf/docker-entrypoint.sh
@@ -9,20 +9,21 @@
 
 
 # DOwnload wordpress and put it into /var/www/html directory
-if [ ! -f /var/www/html/wp-config.php ]; then
-  echo "Downloading WordPress..."
-  curl -o /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
-  echo "Extracting WordPress..."
-  tar -xzf /tmp/wordpress.tar.gz -C /tmp
-  echo "Moving WordPress files to /var/www/html..."
-  mv /tmp/wordpress/* /var/www/html/
-  echo "Setting permissions..."
-  chown -R www-data:www-data /var/www/html
-  chmod -R 755 /var/www/html
-fi
+# if [ ! -f /var/www/html/wp-config.php ]; then
+#   echo "Downloading WordPress..."
+#   curl -o /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
+#   echo "Extracting WordPress..."
+#   tar -xzf /tmp/wordpress.tar.gz -C /tmp
+#   echo "Moving WordPress files to /var/www/html..."
+#   mv /tmp/wordpress/* /var/www/html/
+#   echo "Setting permissions..."
+#   chown -R www-data:www-data /var/www/html
+#   chmod -R 755 /var/www/html
+# fi
 
 #check if wp-config.php exist
 if [ ! -f /var/www/html/wp-config.php ]; then
+  ls -la /var/www/html
   /usr/local/bin/wp-cli.phar config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$MYSQL_HOSTNAME" --path=/var/www/html --allow-root
   /usr/local/bin/wp-cli.phar core install --url=http://"$DOMAIN_NAME" --title="Deep Inception" --admin_user="$WP_SURUSER" --admin_password="$WP_SURPHRASE" --admin_email="$WP_SUREMAIL" --path=/var/www/html --skip-email --allow-root
 fi 
